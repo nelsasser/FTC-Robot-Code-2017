02df67238117c77e1ec4ee9b0d2cd3e73d8d3bc2
nelsasser <elsanic96@gmail.com>
2017-02-10 16:40:55 -0600

    Create KarelAutoBLUE.java

---
+++ KarelAutoBLUE.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cCompassSensor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IrSeekerSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+/**
+ *
+ */
+@Autonomous(name="KarelAutoBLUE", group="Pushbot")
+public class KarelAutoBLUE extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
